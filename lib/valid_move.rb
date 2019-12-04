211d80a5093ed093e01034d0445bae5b2417c5c9
Isaiah Velez <isaiah.velez611@gmail.com>
2019-12-04 02:55:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,8 +9,8 @@ end
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
-    return true
-  elsif board[index] == "X" || board[index] == "O"
     return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
   end
 end
